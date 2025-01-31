258914eb7daf90f1187d3c044ae556798db19bfe
daniel.chaguaro <daniel.chaguaro@udla.edu.ec>
2025-01-31 03:14:44 -0500

    Se incluye el productor a la plataforma de salud

---
+++ Productor/Models/Paciente.cs
@@ -0,0 +1,13 @@
+﻿namespace Productor.Models
+{
+    public class Paciente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public string Edad { get; set; }
+
+        public string Genero { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Productor/Utils/ValidationHelper.cs
@@ -0,0 +1,14 @@
+﻿using Productor.Models;
+
+namespace Productor.Utils
+{
+    public static class ValidationHelper
+    {
+        public static bool EsAnormal(Resultado resultado)
+        {
+            return resultado.Hemoglobina < 12 || resultado.Hemoglobina > 16 ||
+                   resultado.Colesterol > 200 ||
+                   resultado.GlobulosRojos < 4.7 || resultado.GlobulosRojos > 6.1;
+        }
+    }
+}
