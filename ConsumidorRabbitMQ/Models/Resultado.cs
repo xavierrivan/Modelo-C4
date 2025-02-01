7ab9fe96151fe38f76f0cbd288e835850198cfbd
xavie <xavie@XAVIERLAP>
2025-01-31 21:05:20 -0500

    Se sube el consumidor del sistema

---
+++ ConsumidorRabbitMQ/Models/Resultado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumidorRabbitMQ.Models
+{
+    public class Resultado
+    {
+        public int IdPaciente { get; set; }
+        public double Hemoglobina { get; set; }
+        public double Colesterol { get; set; }
+        public double GlobulosRojos { get; set; }
+    }
+}
